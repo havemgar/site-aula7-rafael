e25eaee28b3104ff6771d1e8a735783144ab64a5
havemgar <r-fael@live.com>
2025-06-30 02:14:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let butao = document.querySelector("#butao");
+let mensagemButao = document.querySelector("#mensagemButao")
+
+butao.addEventListener("click", e =>{
+mensagemButao.innerHTML="O palmeiras nao tem mundial!"    
+});
